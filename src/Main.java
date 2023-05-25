78c896e0f36e9e78eccf7c2ff913747689b8cf09
DESKTOP-KKSS5AK\usuario <maiolcarmona@gmail.com>
2023-05-25 20:26:15 +0200

    cambios rama-cambiosPrueba

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("primer cambio en el proyecto");
+        System.out.println("cambios solo en rama-cambiosPrueba");
     }
 }
